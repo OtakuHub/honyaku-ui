12cadd613d4f89c081868b2de410f3067b7adf42
Mycroft1891 <mycroft1891@gmail.com>
2020-08-23 17:50:29 +0200

    Updates

---
+++ precache-manifest.a077bc1afd440276710df2013c0f3637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e5a18888f8abba3b4256cfa3392505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865126ec26df5a98817c",
+    "url": "/static/css/main.fc9d53d9.chunk.css"
+  },
+  {
+    "revision": "be826413548739fe6e73",
+    "url": "/static/js/2.53d5e147.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.53d5e147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865126ec26df5a98817c",
+    "url": "/static/js/main.c43622c1.chunk.js"
+  },
+  {
+    "revision": "9f672db06a55abef940d",
+    "url": "/static/js/runtime-main.6e70271f.js"
+  }
+]);
