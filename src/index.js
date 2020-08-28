e2357be5652cec407ef3d44978f02547476b8e2f
Mycroft1891 <mycroft1891@gmail.com>
2020-08-28 12:41:50 +0200

    Add custom history object to programmaticaly route users

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import history from './history';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={history}>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'),
 );
 
