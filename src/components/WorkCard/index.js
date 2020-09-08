fef33845cf40b9487e7d846c2dcfc545632aa0b9
Mycroft1891 <mycroft1891@gmail.com>
2020-09-09 01:08:53 +0200

    Create a CardList component for rendering a lit of cards responsively

--- src/components/WorkCard/index.js
+++ src/components/WorkCard/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const WorkCard = ({ id, title, coverImage, description, genres }) => (
-  <Col>
-    <Card style={{ minWidth: '15rem', marginTop: '1rem' }}>
-      <Card.Img
-        style={{ height: '15rem', objectFit: 'cover' }}
-        variant="top"
-        src={coverImage}
+  <Card style={{ minWidth: '15rem', marginTop: '1rem' }}>
+    <Card.Img
+      style={{ height: '15rem', objectFit: 'cover' }}
+      variant="top"
+      src={coverImage}
+    />
+    <Card.Body>
+      <Card.Title>
+        {title}
+      </Card.Title>
+      <Card.Subtitle>
+        {genres}
+      </Card.Subtitle>
+      <Card.Text
+        dangerouslySetInnerHTML={{ __html: description.substring(0, 100) }}
       />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Subtitle>{genres}</Card.Subtitle>
-        <Card.Text
-          dangerouslySetInnerHTML={{ __html: description.substring(0, 100) }}
-        />
-        <Card.Link>
-          <Link to={`/work/${id}`}>Read More</Link>
-        </Card.Link>
-      </Card.Body>
-    </Card>
-  </Col>
+      <Card.Link>
+        <Link to={`/work/${id}`}>
+          Read More
+        </Link>
+      </Card.Link>
+    </Card.Body>
+  </Card>
 );
 
 export default WorkCard;
